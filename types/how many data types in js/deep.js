a6d2310451b4a9a1044276978a5c926482b867ff
unknown <mehnazm406@gmail.com>
2024-12-07 13:41:17 +0530

    deep JS

--- types/how many data types in js/deep.js
+++ types/how many data types in js/deep.js
@@ -1,143 +1,134 @@
+// //                      Data types divide into two part
 
+// //                                 Data type
+// //
+// //                 ___________________|___________________
+// //                 |                                      |
+// //              Primitve type                       non-primitve type
+// //                |                                       |
+// //            String,boolean                            Object(object ,array ,function)
+// //            symbol,BigInt
+// //            Nummber,Undefined
+// //             null.
 
-//                      Data types divide into two part
+// //What is between  primitve type and none primitive???.
+// //Primitve type
 
-//                                 Data type
-//      
-//                 ___________________|___________________  
-//                 |                                      |
-//              Primitve type                       non-primitve type
-//                |                                       |
-//            String,boolean                            Object(object ,array ,function)
-//            symbol,BigInt                  
-//            Nummber,Undefined
-//             null.
+// //-Baisc data types.
 
+// //-immutable(immutable means one is created never be change) data types.
+// var str = "Hello";
+// str ="world";//you can change str value direclty
+// console.log(str);
+// //-directly store in the memoery.
 
-//What is between  primitve type and none primitive???.
-//Primitve type
+// //non-primitve-type/reference type
 
-//-Baisc data types.
+// //-mutable(means u can change after one created)data types.
+// //example
+// var arr = [1,2,3];
+// arr.push(4);//change original arr
+// console.log(arr);
 
-//-immutable(immutable means one is created never be change) data types.
+// //-direclty no store in momory.
 
-//-directly store in the memoery.
+// //primitve types
+// //Number:-
+// //-Represent both integer and floating point numbera.They are both of types 'number'.
 
+// //example
+// var x= 2;
+// var y =9.001;
+// var z = -78;
 
-//non-primitve-type/reference type
+// //String:-
+// //All the textul all is string in js ,string can be in enclosed with single quotes(''),double quotes("") and backticks(``).
 
-//-mutable(means u can change after one created)data types.
+// var s = 'Hello!';//single quotes
+// var s = "Hello!";//double quotes
+// var s = `Hello!`//backtikcs
 
-//-direclty no store in momory.
+// //Boolean:-
+// //Represent two type false and true.
 
+// var isActive = true;
+// var isPalindrome= false;
 
-//primitve types
-//Number:-
-//-Represent both integer and floating point numbera.They are both of types 'number'.
+// //Symbol:-
 
-//example
-var x= 2;
-var y =9.001;
-var z = -78;
+// //syombol is a primitive data types introduced into ECMAScript2015(ES6).
 
+// //symbol data type is unique value and immtuble data type in js
 
-//String:-
-//All the textul all is string in js ,string can be in enclosed with single quotes(''),double quotes("") and backticks(``).
+// //symbol is often use for object key to avoid name conllision.
 
-var s = 'Hello!';//single quotes
-var s = "Hello!";//double quotes
-var s = `Hello!`//backtikcs
+// var id = Symbol("id");
+// var preson = {
+//     name:"john",
+//     age:31,
+//     [id] :'Johngamil@.com',
+// }
+// console.log(preson[id]);
+// console.log(preson);
 
-//Boolean:-
-//Represent two type false and true.
+// for(let key in preson){
+//     console.log(key);
+// }
 
-var isActive = true;
-var isPalindrome= false;
+// var message = Symbol("Hello world!");
+// console.log(message.description);
+// console.log(message.toString());
+// console.log(message);
 
+// var a = Symbol("Hello!")
+// var b = Symbol("Hello!")
+// console.log(a===b);//false because each symbol is unique.
 
-//Symbol:-
+// //undefined:-
+// //undefined represent the variable dose not have any value in current time.
+// //do not worry! explain better way
+// // let's understood with example:-
 
-//syombol is a primitive data types introduced into ECMAScript2015(ES6).
+// var x;//when you declare a variable but not assing any value means that is undefined.
+// console.log(x);//undefined
 
-//symbol data type is unique value and immtuble data type in js
+// //memeory
 
-//symbol is often use for object key to avoid name conllision.
+// //  -----------------
+// // |                 |
+// // | x : undefined   |
+// // |                 |
+// //  -----------------
 
-var id = Symbol("id");
-var preson = {
-    name:"john",
-    age:31,
-    [id] :'Johngamil@.com',
-}
-console.log(preson[id]);
-console.log(preson);
+// //when javascript see user dose not assinge any value so javascript automatically assing undefined(represent variable is declare but no assing any value) and undefined is default value.
 
-for(let key in preson){
-    console.log(key);
-}
+// //null:-
+// //-null is a primitive data types.
 
-var message = Symbol("Hello world!");
-console.log(message.description);
-console.log(message.toString());
-console.log(message);
+// //It is assigment value that intentional represent absecne of any object  value.
 
+// //null is special value in js that  intentionaly indaicting variable should hold an object at moment ,it dose not.means simple represent "nothing" and "no object".
 
+// //BigInt:-
 
-var a = Symbol("Hello!")
-var b = Symbol("Hello!")
-console.log(a===b);//false because each symbol is unique.
+// //-all the integer greater then 2**52-1  handle in bigInt type.
 
+// //How to create
+// var bigInt = 12n;
+// var bigInt = BigInt("12");
+// console.log(bigInt);
 
+// console.log(7n/7n);//1n
+// console.log(7n+7n);//14n
+// console.log(7n-7n);//0n
 
-//undefined:-
-//undefined represent the variable dose not have any value in current time.
-//do not worry! explain better way
-// let's understood with example:-
+// console.log(Math.min(2,3));
+// console.log(13n/4n);//3
+// console.log(5n/2n);//2
 
-var x;//when you declare a variable but not assing any value means that is undefined.
-console.log(x);//undefined
+// //javascript dose not converte decimal value to bigInt
+// // var decimalValue = 2.3n; //erroe!
 
-//memeory
+// // console.log(Math.min(2n,3n));//Error!
 
-//  -----------------
-// |                 |
-// | x : undefined   |
-// |                 |
-//  -----------------
-
-//when javascript see user dose not assinge any value so javascript automatically assing undefined(represent variable is declare but no assing any value) and undefined is default value.
-
-
-//null:-
-//-null is a primitive data types.
-
-//It is assigment value that intentional represent absecne of any object  value.
-
-//null is special value in js that  intentionaly indaicting variable should hold an object at moment ,it dose not.means simple represent "nothing" and "no object".
-
-
-//BigInt:-
-
-//-all the integer greater then 2**52-1  handle in bigInt type.
-
-//How to create
-var bigInt = 12n;
-var bigInt = BigInt("12");
-console.log(bigInt);
-
-console.log(7n/7n);//1n
-console.log(7n+7n);//14n
-console.log(7n-7n);//0n
-
-console.log(Math.min(2,3));
-console.log(13n/4n);//3
-console.log(5n/2n);//2
-
-
-
-//javascript dose not converte decimal value to bigInt
-// var decimalValue = 2.3n; //erroe!
-
-// console.log(Math.min(2n,3n));//Error!
-
-// console.log(typeof +2n);//Error!
+// // console.log(typeof +2n);//Error!
--- types/typeof operator/deep.js
+++ types/typeof operator/deep.js
@@ -13,3 +13,26 @@ console.log(typeof x);//'number'
 //|                  |
 //| x:2 ->number     |
 //| ----------------- 
+//it is go into momory and check where is x store, find x locaition and which value hold x variable see it hold value is 2 that is integer ,means that is a number.
+
+//Type of operator
+//- The typeof operator always return result in string form.
+
+//-It check which value type value store in variables.
+
+//do practice with given example
+var value;
+console.log(typeof 42);//??
+console.log(typeof "");//??
+console.log(typeof 'hello');//??
+console.log(typeof true);//??
+console.log(typeof undefined);//??
+console.log(typeof false);//??
+console.log(typeof 0);//??
+console.log(typeof {});//??
+console.log(typeof []);//??
+console.log(typeof -7);//??
+console.log(typeof -7n);//??
+console.log(typeof `-7n`);//??
+console.log(typeof 2.3);//??
+console.log(typeof `${2**2}`);//??
