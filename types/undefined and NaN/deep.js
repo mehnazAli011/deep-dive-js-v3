94be42bb1ab1ae55165d8d1e55d1c49e07fe2339
unknown <mehnazm406@gmail.com>
2024-12-16 20:03:00 +0530

    Deep JS

---
+++ types/undefined and NaN/deep.js
@@ -0,0 +1,21 @@
+
+//Undfined
+//undefined vs undeclared vs uninitliaztion.
+
+//undefined means definitly a variable but at a momnet but not have any value.
+
+//undeclared measn this value does not create in any scope.
+
+//uninitliazation and TDZ (Tempreal dead zone) was introduced with E56,in simple trem you meaning you can not touch yet.
+
+
+
+
+//NaN &&isNaN
+
+//-NaN represent invalid number in javascript.
+
+//-In some cases javascript return NaN.
+
+//NaN is not eqaul itslef because NaN is unique value in js.
+console.log(NaN===NaN);//false
