efced092252895cad7ce4ce0233291614c2d09f7
unknown <mehnazm406@gmail.com>
2024-12-17 21:58:35 +0530

    Deep JS

--- types/undefined and NaN/deep.js
+++ types/undefined and NaN/deep.js
@@ -19,3 +19,7 @@
 
 //NaN is not eqaul itslef because NaN is unique value in js.
 console.log(NaN===NaN);//false
+
+//wheh you try converted non-numerical value to number type
+var str = "Hello";
+console.log(Number(str));//NaN means in valid operation in js
