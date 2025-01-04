ce43e577dfcf6e6c5ebba2d20a08066a7c0d9343
unknown <mehnazm406@gmail.com>
2025-01-04 22:23:30 +0530

    Deep JS

---
+++ types/typeof.js
@@ -0,0 +1,19 @@
+
+//what is typeof operator???
+
+//The typeof operator in js  used to  check  type of variable.
+
+
+//let's see 
+var x = 2;
+console.log(typeof x);//'number'
+
+
+//How it is a work??
+
+// ------------------
+//|                  |
+//| x:2 ->number     |
+//| ----------------- 
+
+//it is go into momory and check where is x store, find x locaition and which value hold x variable see it hold value is 2 that is integer ,means that is a number.
--- types/undefined and NaN/deep.js
+++ types/undefined and NaN/deep.js
@@ -98,12 +98,3 @@ console.log(isNaN(123));
 //true
 //true
 
-
-//what is typeof operator???
-
-//The typeof operator in js  used to  check  type of variable.
-
-
-//let's see 
-var x = 2;
-console.log(typeof x);//'number'
