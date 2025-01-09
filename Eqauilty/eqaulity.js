2f91ce98c942e3bef0749408e563533c2d9b7142
unknown <mehnazm406@gmail.com>
2025-01-09 21:21:10 +0530

    deep JS

--- Eqauilty/eqaulity.js
+++ Eqauilty/eqaulity.js
@@ -7,3 +7,18 @@
 var x = 3;
 var y ="3";
 console.log(x==y);//true ???
+
+
+
+
+//behind the scenes
+// memory   |check value | type     |   type conversion |after     | comparision     |result
+//                                                     type conversion
+// x=3      |  3         | number   |   no-conversion   |  3-number |  3==3          |true
+//y="3"     |  3         |string    |   toNumber("3")   |  3-number |                |
+
+
+
+var x= 3;
+var y ="3";
+console.log(x===y);//false
