6b53b50148455568fada634879de3cec11a6400f
unknown <mehnazm406@gmail.com>
2025-01-26 22:51:42 +0530

    deep JS

--- scope/scope.js
+++ scope/scope.js
@@ -2,3 +2,12 @@
 //scope is refer to the context in which a variable is accessible.
 
 //In simple trem i can say scope is deretmine where you can access  variable and function in our code.
+
+//scope are four type in js
+
+//global scope:-
+//variable declare outside of any function and block means it is in the global scope and this variable is accessible everywhere in  our code.
+
+//example
+var x = 2; //global scope
+console.log(x); //x is accessible everywhere
