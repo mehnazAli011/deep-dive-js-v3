aecc854ec35b884970a6b4a1abd1175ca5491b82
mehnaz <mehnazm406@gmail.com>
2025-02-09 23:39:25 +0530

    Update eqaulity.js

--- Eqauilty/eqaulity.js
+++ Eqauilty/eqaulity.js
@@ -1,8 +1,8 @@
-//Double Equlaity                  VS               Triple Equality
+Double Equlaity                  VS               Triple Equality
 
-//In simple trem Double equality check both type and value after convert both type to common type,while triple equality check both type and value without any type conversion.
+In simple trem Double equality check both type and value after convert both type to common type,while triple equality check both type and value without any type conversion.
 
-//let's understand with example
+let's understand with example
 
 var x = 3;
 var y ="3";
