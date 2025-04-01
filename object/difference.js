348dab16955f749f3fd9bdd0f11071a4ff18a00a
unknown <mehnazm406@gmail.com>
2025-04-01 22:46:49 +0530

    arrow and regular function

---
+++ object/difference.js
@@ -0,0 +1,15 @@
+        What is different between the arrow function and regular function?
+
+1. Syntax and Return Behavior
+
+Arrow Function:
+
+Syntax: let sum = (a, b) => a + b;
+​
+Explanation: Arrow functions provide a concise syntax. When the function consists of a single expression, the return statement is implicit; there's no need to use the return keyword. ​
+
+Regular Function:
+
+Syntax: ```javascript function sum(a, b) { return a + b; }
+
+Explanation: Traditional functions require the use of the function keyword and explicitly include the return statement to output a value.
