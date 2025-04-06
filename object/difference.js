e5da0bd8219e1abdfa4f183a9e2d807905941d58
unknown <mehnazm406@gmail.com>
2025-04-06 21:21:46 +0530

    differences

--- object/difference.js
+++ object/difference.js
@@ -1,6 +1,7 @@
 //         What is different between the arrow function and regular function?
+//1=>
 
-// 1. Syntax and Return Behavior
+// Syntax and Return Behavior
 
 // Arrow Function:
 
@@ -13,6 +14,7 @@
 // Syntax: ```javascript function sum(a, b) { return a + b; }
 
 // Explanation: Traditional functions require the use of the function keyword and explicitly include the return statement to output a value.
+//2=>
 
 //Arrow Function
 
@@ -31,16 +33,14 @@ function regularFunc() {
 
 // > Because of the design, arrow function is designed lexically; it only depends on where the function is written, and regular function is designed dynamically; it only depends on where the function is called.
 
-
 const obj = {
-    name: "My Object",
-    arrowFunc: () => {
-      console.log(this.name);  // here "this" refers window not obj
-    }
-  };
-  obj.arrowFunc(); 
+  name: "My Object",
+  arrowFunc: () => {
+    console.log(this.name); // here "this" refers window not obj
+  },
+};
+obj.arrowFunc();
 console.log(obj.arrowFunc());
-
 
 //   const obj = {
 //     name: "My Object",
@@ -50,27 +50,26 @@ console.log(obj.arrowFunc());
 //   };
 //   obj.regularFunc();  // My Object
 
+//3=>
 
 // Arrow function
 // Arrow function do not use as a constructed because arrow function do not have[[construct]]  internal method.
 
-
 // Regular Function
 // Regular funtion use as a constructer it's call with the new keyword.
-// When used with new, they create an object and bind the this value to the newly created object. 
+// When used with new, they create an object and bind the this value to the newly created object.
 
+//4=>
+// Arrow Function
+// Arrow function do not have argumnet object.
+// const arrowFunc = () => {
+//   console.log(arguments); // Will throw an error
+// };
 
-Arrow Function
-Arrow function do not have argumnet object.
-const arrowFunc = () => {
-  console.log(arguments); // Will throw an error
-};
+// Regular Function
+// Regular function have arguments object which is an array-like object that contains all arguments passed to the function.
 
-Regular Function
-Regular function have arguments object which is an array-like object that contains all arguments passed to the function.
-
-
-function sum() {
-  console.log(arguments); // {0: 1, 1: 2, 2: 3, ...}
-}
-sum(1, 2, 3); // Logs arguments
+// function sum() {
+//   console.log(arguments); // {0: 1, 1: 2, 2: 3, ...}
+// }
+// sum(1, 2, 3); // Logs arguments
