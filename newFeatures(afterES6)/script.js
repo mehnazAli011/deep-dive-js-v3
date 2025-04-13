6580b64cfd5daa0bcdfbf2c25f44f74916047ccc
unknown <mehnazm406@gmail.com>
2025-04-13 22:39:25 +0530

    featurea

--- newFeatures(afterES6)/script.js
+++ newFeatures(afterES6)/script.js
@@ -1,6 +1,17 @@
 
-What is primvate variable why it's need this(After ES6)
-Private variable is only accesble inside of class and function synatax.
-Before ES6,JS didn't have a real class syntax (like other lanagues python,java etc).
+// What is primvate variable why it's need this(After ES6)
+// Private variable is only accesble inside of class and function synatax.
+// Before ES6,JS didn't have a real class syntax (like other lanagues python,java etc).
 
-After ES6 introduces classes so devloper want hide internal details of class.
+// After ES6 introduces classes so devloper want hide internal details of class.
+
+
+//Before ES6
+// class User {
+//   constructor(name) {
+//     this.name = name;
+//     this.password = "12345";
+//   }
+// }
+// // const user = new User("John");
+// console.log(user.password); //everyone access  the passwrod.
